d1a585925f353df8ef9e23d7045862a2ae6182a8
FeruYasu <fernando_yasumoto@hotmail.com>
2023-06-08 09:37:57 -0300

    Added jsonDB

---
+++ src/index.ts
@@ -0,0 +1,68 @@
+import express, { Request, Response, NextFunction } from "express";
+
+import { Server } from "socket.io";
+import http from "http";
+import { JsonDB, Config } from "node-json-db";
+const { v4: uuidv4 } = require("uuid");
+
+const PORT = 8899;
+const app = express();
+// app.use(express.static(__dirname + '/../client'))
+const httpServer = http.createServer(app);
+const io = new Server(httpServer);
+
+const clients: Array<any> = [];
+const senhas: Array<string> = [];
+
+var db = new JsonDB(new Config("jsonDB", true, false, "/"));
+
+app.use(express.json());
+
+app.post("/user", async (request: Request, response: Response) => {
+  const { name } = request.body;
+
+  let userData = [];
+
+  try {
+    userData = await db.getData("/users");
+  } catch (error) {}
+
+  const newUser = {
+    id: uuidv4(),
+    name,
+    status: "queue",
+    senha: userData.length.toString().padStart(4, "0"),
+  };
+
+  await db.push("/users[]", newUser);
+
+  return response.status(201).json(newUser);
+});
+
+io.on("connection", (client) => {
+  console.log("Cliente conectado");
+  clients.push(client);
+
+  client.on("senhas", function (socket) {
+    for (const client of clients) {
+      client.emit("msg", senhas);
+    }
+  });
+
+  client.on("add", function (socket) {
+    senhas.push(socket.senha);
+  });
+
+  client.on("remove", function (socket) {
+    senhas.splice(senhas.indexOf(socket.senha), 1);
+  });
+
+  client.on("disconnect", () => {
+    console.log("Cliente desconectado");
+    clients.splice(clients.indexOf(client), 1);
+  });
+});
+
+httpServer.listen(PORT, () => {
+  console.log(`Server started at ${PORT}`);
+});
